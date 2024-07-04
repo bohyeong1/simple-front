89b102fcafdc94861f4ba72cc7dd350026875034
서보형 <tjqhgud9104@naver.com>
2024-07-05 01:18:44 +0900

    saramin_example

--- example1/example1.js
+++ example1/example1.js
@@ -1,8 +1,130 @@
-document.addEventListener('click', function(event) {
-    const dropdowns = document.querySelectorAll('select')
-    dropdowns.forEach(dropdown => {
-        if (!dropdown.contains(event.target)) {
-            dropdown.blur()
+const dropdowns = document.querySelectorAll('#select')
+
+///외부 클릭 시 드롭다운 닫히기
+document.addEventListener('click', (e) => {
+    dropdowns.forEach(el => {
+        if (!el.contains(e.target)) {
+            if(el.tagName === 'SELECT'){
+                el.blur()
+            }else if(el.tagName === 'DIV'){
+                custom_dr.classList.remove('logo_active')
+                custom_options_container.classList.remove('active')
+                custom_options_container.dataset.state = 'false'
+                enter_state = false
+            }
         }
     })
 })
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////custom
+const custom_dr = document.querySelector('.select-container__custom-logo')
+const custom_options = Array.from(document.querySelectorAll('.select-container__list')) 
+const custom_options_container = document.querySelector('.select-container__custom-options')
+const custom_dr_logo = document.querySelector('.select-container__custom-logo span')
+// console.log(custom_options)
+let enter_state = false        ///포커스 후 enter키 눌렀을 시 키보드 입력가능
+let focus_option = -1        ///처음 포커스 되있는 메뉴리스트, 호버 안했을 시 기본값 = 첫번째
+let list_focus_state = false         /////기본 제공되는 select tag는 처음 엔터 입력 후 키보드 위키 눌렀을 시 포커스 안된는 거랑 똑같이 구현하기 위해서 만든 스테이트
+
+// 드롭다운 열/닫기
+custom_dr.addEventListener('click', ()=>{
+    if(!enter_state){
+        enter_state = true
+        custom_dr.classList.add('logo_active')
+        custom_options_container.classList.add('active')
+        custom_options_container.dataset.state = 'true'
+    }else{
+        enter_state = false
+        custom_dr.classList.remove('logo_active')
+        custom_options_container.classList.remove('active')
+        custom_options_container.dataset.state = 'false'
+    }
+})
+
+// 드롭다운 옵션 선택
+custom_options.forEach((el)=>{
+    el.addEventListener('click', (e)=>{
+        custom_options.forEach((ele)=>{
+            ele.classList.remove('click_active')
+        })
+        e.target.classList.toggle('click_active')
+        custom_dr_logo.textContent = e.target.dataset.value
+        custom_options_container.classList.toggle('active')
+        custom_dr.classList.toggle('logo_active')
+        list_focus_state = true
+        enter_state = false
+        custom_options_container.dataset.state = 'false'
+        focus_option = custom_options.indexOf(e.target)
+    })
+})
+
+
+custom_dr.addEventListener('keydown', (()=>{
+    const length = custom_options.length   ////////메뉴 리스트 개수
+    return function(e){
+        // 포커스 후 엔터눌렀을 때
+        if(e.key === 'Enter' && !enter_state){        //엔터 눌러서 드롭다운 처음 열 때 // focus_option, list_focus_state 한번 초기화 때려주기
+            custom_options_container.dataset.state = 'true'
+            custom_dr.classList.add('logo_active')
+            custom_options_container.classList.add('active')
+            enter_state = true
+        }else if(e.key ==='Enter' && enter_state){  //드롭다운 열렸을 때 엔터 눌렀을 시 // 클릭으로 들어와서 키보드로 나가는 경우대비해서 focus_option, list_focus_state, enter_state 초기화
+            custom_options_container.dataset.state = 'false'
+            enter_state = false
+            custom_dr.classList.remove('logo_active')
+            custom_options_container.classList.remove('active')
+        }
+        
+        //////////////////////키보드 위키 눌렀을 때 //초기에는 안들어옴
+        else if(e.key === 'ArrowUp' && enter_state && list_focus_state){
+            if(focus_option === -5){ /// -5미만으로 떨어지면 focus_option이 감소 할때 절대값은 증가되므로 키보드 방향이 바뀌기 때문에 0으로 초기화시켜서
+                                     ///   focus_option이 0~ -5까지만 돌게하기 ... 그다지 좋은 방법은 아닌듯 추후 더 좋은 방법이 생각 해보기
+                focus_option = 0
+            }
+            focus_option--
+            const index = length + focus_option
+            console.log(index)
+            custom_options[Math.abs(index + 1) % length].classList.remove('click_active') ////////그전에 선택된 스타일 삭제
+            custom_options[Math.abs(index) % length].classList.add('click_active')
+            custom_dr_logo.textContent = custom_options[Math.abs(index) % length].dataset.value
+        }
+        //////////////////////키보드 아래키 눌렀을 때
+        else if(e.key ==='ArrowDown' && enter_state && !list_focus_state){ ////////////포커스 된 후 첨 아래키 눌렀을 때
+            focus_option++
+            const index = length + focus_option
+            custom_options.forEach((el)=>{
+                el.classList.remove('click_active')
+            })
+            list_focus_state = true
+            custom_options[index % length].classList.add('click_active')
+            custom_dr_logo.textContent = custom_options[index % length].dataset.value
+        }
+        else if(e.key ==='ArrowDown' && enter_state && list_focus_state){///////////// 포커스 된 후 두번째 부터ㅇㅇ
+            focus_option++
+            const index = length + focus_option
+            // console.log(index)
+            // console.log(focus_option)
+            custom_options[Math.abs(index - 1) % length].classList.remove('click_active') ////////그전에 선택된 스타일 삭제
+            custom_options[Math.abs(index)  % length].classList.add('click_active')
+            custom_dr_logo.textContent = custom_options[Math.abs(index)  % length].dataset.value
+        }
+    }
+
+})())
+
+
+////////////////////////////메뉴리스트에 마우스  enter 시 선택된 메뉴 스타일 초기화
+
+custom_options_container.addEventListener('mouseenter',()=>{
+    // console.log('확인')
+    // console.log(custom_options_container.dataset.state)
+    if(custom_options_container.dataset.state === 'true'){
+        custom_options.forEach((el)=>{
+            el.classList.remove('click_active')
+        })
+    }
+
+})
+
+
--- example4/example4.js
+++ example4/example4.js
@@ -1,7 +1,7 @@
 const start_btn = document.querySelector('.quiz-container__start-btn')
-const collect_btn = document.querySelector('.quiz-container__btn.collect')
-const wrong_btn = document.querySelector('.quiz-container__btn.wrong')
+const select_btn = Array.from(document.querySelectorAll('.quiz-container__btn')) 
 const timer_board = document.querySelector('.quiz-container__timer-board')
+const log = document.querySelector('.quiz-container__log') /// 상태값 출력되는 로그창
 // 타이머
 let timer = null
 
@@ -11,9 +11,10 @@ function timer_callback(time){
     let input_time = time
     timer = setInterval(()=>{
         input_time--
-        timer_board.textContent = input_time
+        timer_board.textContent = `${String(Math.floor(input_time/60)).padStart(2,0)}:${String(Math.floor(input_time%60)).padStart(2,0)}`
         if(input_time === 0){
             clearInterval(timer)
+            timer = null
         }
     },1000)
 }
@@ -22,25 +23,100 @@ function timer_callback(time){
 ////////////스크립트 함수 (0.3초마다 반복 호출해서 타이머를 구현하는건지 or 제한시간을 체크하는건지 잘 모르겠어서 제한시간을 기준으로 구현 하였습니다.)
 const startQuestionTimer = (function(){
     let check_time = 0                   ////////////////0.3초마다 제한시간이 됬나 체크하는 변수
+    let start_state = false              //////////////start 버튼 클릭 유무 나타내는 스테이트 -> 재귀적으로 반복 실행되는 로직 컨트롤
+    let input_time = 0                  /////////////////파라미터로 들어온 time값 저장 하는 곳 / 퀴즈 종료 시 퀴즈 시간 변경
+    let recursion_timer = null              /////////재귀함수 돌리는 settime 타이머
+    let first_render = false               ////////////초기값 설정 스테이트
     return function (callback, time){
         if(time>0 && typeof time === 'number' && Number.isInteger(time)){
+            
+            ////////스타트 버튼 눌렀을 때 실행
+            if(start_state){
+                // 0.3초마다 시간누적시켜서 0초에 도달했나 체크 // 
+                // 시간이 3의 배수가 아닐 시 오차 0.1초생김(ex. 10초 => 9.9 + alpa(스크립트 읽는 시간) 초)
 
-            // console.log(check_time)
-            if(!timer){
-                callback(time)
-            }else{// 0.3초마다 시간누적시켜서 0초에 도달했나 체크 // 
-                  // 시간이 3의 배수가 아닐 시 오차 0.1초생김(ex. 10초 => 9.9 + alpa(스크립트 읽는 시간) 초)
                 check_time += 0.3 
-                if((time - check_time) < check_time && Math.abs((time - check_time)) < Math.abs((time - check_time - 0.3)) ){
+                if((input_time - check_time) < check_time && Math.abs((input_time - check_time)) < Math.abs((input_time - check_time - 0.3)) ){
                     console.log(check_time)
                     check_time = 0
-                    timer_board.textContent = time
-                    alert('제한시간 초과')
+                    input_time = time/2
+                    timer_board.textContent = `${String(Math.floor(input_time/60)).padStart(2,0)}:${String(Math.floor(input_time%60)).padStart(2,0)}`
+                    start_state = false
+                    log.textContent = '제한시간 초과'
+                    start_btn.textContent = 'RESTART'
+                    clearInterval(timer)
+                    clearTimeout(recursion_timer)
+                    timer = null
+                    recursion_timer=null
+
+                    // clearTimeout(recursion_timer)
+
                     return //재귀 끝
                 }
+                recursion_timer = setTimeout(()=>{startQuestionTimer(callback,time)}, 300)
+            }else{
+                // 초기 렌더링 시 클릭 이벤트 처리 / 재귀함수 실행 시 중복 이벤트 등록되지 않게 start_state = false 일 때 등록
+                if(!first_render){
+                    first_render = true
+
+                        /////////초기 제한 시간 저장
+                    input_time = time
+
+                    ///초기 input으로 들오언 time(시간) 화면에 렌더링
+                    timer_board.textContent = `${String(Math.floor(input_time/60)).padStart(2,0)}:${String(Math.floor(input_time%60)).padStart(2,0)}`
+
+                    console.log('이벤트 등록 횟수 체크')
+
+                    // 시작하기 btn click event 등록
+                    start_btn.addEventListener('click', (() => {
+                        console.log(input_time)
+                        log.textContent = '로그 출력'
+                        timer_board.textContent = `${String(Math.floor(input_time/60)).padStart(2,0)}:${String(Math.floor(input_time%60)).padStart(2,0)}`
+                        check_time = 0
+                        clearInterval(timer)
+                        clearTimeout(recursion_timer)
+                        recursion_timer=null
+                        timer = null
+                        callback(input_time)
+                        start_state = true
+                        setTimeout(()=>{startQuestionTimer(callback,time)}, 300) ///////start 버튼 클릭 시 재귀 첫 실행
+
+                    }))
+
+                    ///////정답 btn click event 등록
+                    select_btn.forEach((el)=>{
+                        el.addEventListener('click',(e)=>{
+                            if(!timer){
+                                alert('시작하기 버튼을 눌러주세요')
+                                return
+                            }
+                            if(e.target.dataset && e.target.dataset.collect === 'true'){
+                                log.textContent = '정답입니다'
+                                clearInterval(timer)
+                                timer = null
+                                clearTimeout(recursion_timer)
+                                recursion_timer=null
+                                input_time = time/2
+                                timer_board.textContent = `${String(Math.floor(input_time/60)).padStart(2,0)}:${String(Math.floor(input_time%60)).padStart(2,0)}`
+                                start_state=false
+                                check_time = 0
+
+                            }else{
+                                log.textContent = '틀렸습니다'
+                                clearInterval(timer)
+                                timer = null
+                                clearTimeout(recursion_timer)
+                                recursion_timer=null
+                                input_time = time/2
+                                timer_board.textContent = `${String(Math.floor(input_time/60)).padStart(2,0)}:${String(Math.floor(input_time%60)).padStart(2,0)}`
+                                start_state=false
+                                check_time = 0
+                            }
+                        })
+                    })
+                }                
             }
-    
-            setTimeout(()=>{startQuestionTimer(callback,time)}, 300)
+
         }else{
             alert('1이상 정수를 입력해 주세요!')
         }
@@ -49,8 +125,8 @@ const startQuestionTimer = (function(){
 
 
  
+///////////////////////파라미터로 받은 제한시간 초기 렌더링시 출력하기 위해 DOMContentLoaded이벤트에 연결
+document.addEventListener('DOMContentLoaded', ()=>{startQuestionTimer(timer_callback,10)})
 
 
 
-// 시작하기 btn click event 등록
-start_btn.addEventListener('click', ()=>{startQuestionTimer(timer_callback,10)})
